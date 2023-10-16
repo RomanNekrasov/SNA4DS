cc5dc0af035b114b3ed41fda651d0a8e798e6a78
Nekrasov <roman.nekrasov@centric.eu>
2023-10-16 23:20:22 +0200

    first test branch

--- constants.py
+++ constants.py
@@ -5,3 +5,9 @@ SNADATAROOT = os.path.join(pc.GOOGLEDRIVEROOT, 'SNA4DS', 'Data')
 SBMDATAROOT = os.path.join(pc.GOOGLEDRIVEROOT, 'SBM', 'Data')
 
 YOUTUBEAPIKEY = pc.YOUTUBEAPIKEY
+
+# Will delete below later, made it as a test to see if the constants.py file works. Check yourself also.
+# make dataframe of google data_first_sample.csv in SBMDATAROOT
+# import pandas as pd
+# df = pd.read_csv(os.path.join(SBMDATAROOT, 'google_data_first_sample.csv'))
+# print(df.head())
