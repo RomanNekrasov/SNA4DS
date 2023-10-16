1dc66da427abf749e88286e29f77068fb6c41daf
Roman Nekrasov <33453661+RomanNekrasov@users.noreply.github.com>
2023-10-16 22:35:11 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,7 @@
+import os 
+import personalConstants as pc
+
+SNADATAROOT = os.path.join(pc.GOOGLEDRIVEROOT, 'SNA4DS', 'Data')
+SBMDATAROOT = os.path.join(pc.GOOGLEDRIVEROOT, 'SBM', 'Data')
+
+YOUTUBEAPIKEY = pc.YOUTUBEAPIKEY
