eff8065848f513365ea33c0ede16d2d0079cbbca
huub <huubvandevoort@home.nl>
2023-10-17 11:31:27 +0200

    Untracked some files that were not ignored

--- personalConstants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# so as an example MY path on MY computer to the google drive folder of our group is: G:\My Drive\JADS group 1
-# as an example I filled in below but you should change it to YOUR path
-# we use os join so Mac and Windows can work happy together
-# once again this file is in the gitignore so it does not track changes you do on your own machine, make sure to import the personal constant in the global constants.py
-GOOGLEDRIVEROOT = os.path.join('G:', 'My Drive', 'JADS group 1')
-
-YOUTUBEAPIKEY = "AIzaSyDKyW4Cekf8K3qakl_seuphE54xscEtJP4"
