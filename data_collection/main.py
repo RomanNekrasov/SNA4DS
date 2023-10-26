486a4550c477251b57c75c25679038a947c9a15d
huub <huubvandevoort@home.nl>
2023-10-26 15:48:32 +0200

    Big code refactor, included argparse for reading multiple video ids

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,6 +1,10 @@
 import datetime
+import sys
+
 import pandas as pd
 import os
+import logging
+import argparse
 
 from config import constants as c
 from collect_edge_data import collect_comments
@@ -14,13 +18,18 @@ from helpers import chunks
 # TODO: Check if the regex expression in collect_edge_data.py is correct
 
 
-def main(videoId, edge_df=pd.DataFrame(columns=['comment_id', 'threath_id', 'time', 'kind', 'author_id',
+def main(video_id: str | list, edge_df=pd.DataFrame(columns=['comment_id', 'threath_id', 'time', 'kind', 'author_id',
                                                 'dest_scraped', 'likes', 'num_replies', 'text', 'video_id']),
          vertex_df=pd.DataFrame(columns=['author_id', 'display_title', 'customer_url', 'member_since',
                                          'subscriber_count', 'view_count', 'video_count'])):
-    # function that fills the edge dataframe
-    edge_df = collect_comments(edge_df=edge_df, videoId=videoId)
 
+    if isinstance(video_id, str):
+        video_id = list(video_id)
+
+    for v_id in video_id:
+        # function that fills the edge dataframe
+        logging.info(f"...Collecting data from {v_id}...")
+        edge_df = collect_comments(edge_df=edge_df, videoId=v_id)
 
     # getting unique authorIds from the edge df
     all_authorIds = list(edge_df['author_id'].unique())
@@ -32,13 +41,22 @@ def main(videoId, edge_df=pd.DataFrame(columns=['comment_id', 'threath_id', 'tim
     # matching the scraped handles of the receivers with the actual AuthorIds
     edge_df, vertex_df = match_receiver_id(edge_df=edge_df, vertex_df=vertex_df)
 
-    newdir = f'scraped-{videoId}-{datetime.datetime.now().strftime("%H.%M %d-%m-%Y")}'
+    newdir = f'test_data/scraped-{datetime.datetime.now().strftime("%H.%M %d-%m-%Y")}'
     os.mkdir(newdir)
-    vertex_df.to_csv(f'{newdir}/vertex_df-{videoId}.csv', index=False)
-    edge_df.to_csv(f'{newdir}/edge_df-{videoId}.csv', index=False)
-    print('executed')
+    vertex_df.to_csv(f'{newdir}/vertex_df.csv', index=False)
+    edge_df.to_csv(f'{newdir}/edge_df.csv', index=False)
+    logging.info("...executed...")
 
 
-# main function call
-# The videoId is the last part of the youtube url
-main(videoId="1pWjP9QNLcg")
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--video_id', type=str, nargs='+')
+    args = parser.parse_args()
+    return vars(args)
+
+
+if __name__ == "__main__":
+    # main function call
+    # The video_id is the last part of the youtube url
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    main(video_id=parse_command_line_arguments().get('video_id', 'nan'))
