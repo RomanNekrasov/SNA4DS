cffe8c8169b00bc664d5250a960e2b404cdca25b
huub <huubvandevoort@home.nl>
2023-12-04 10:26:44 +0100

    Changed the plot graph R script made the data preparation notebook more modulair by setting a filtering functionality in the first cell

--- data_analysis/plot_graph.R
+++ data_analysis/plot_graph.R
@@ -1,9 +1,21 @@
-fp = file.path('data_preparation', 'prepared_data', 'network.graphml')
-graph <- igraph::read_graph(
+fp = file.path("/Users/huubvandevoort/Desktop/SNA4DS/6.Project/SNA4DS/data_preparation/prepared_data/network1.graphml")
+main_object <- igraph::read_graph(
   fp,
 #   '/Users/huubvandevoort/Desktop/SNA4DS/6. Project/SNA4DS/data_preparation/prepared_data/network.graphml',
   format = "graphml"
 )
+
+# filter on edges that have edge weight > 1
+to_delete <- as.vector(igraph::E(graph)[igraph::E(graph)$edge_weight <= 1])
+filtered_graph <- igraph::delete_edges(graph, to_delete)
+
+graph <- main_object
+
+# only taking biggest component
+# components <- igraph::components(graph)
+# largest_component <- which.max(components$csize)
+# graph <- igraph::induced_subgraph(graph, 
+#                                     vids = which(components$membership == largest_component))
 
 # # Generate colors based on year:
 colrs <- c("red", "green", "orange", "purple")
@@ -18,7 +30,6 @@ igraph::V(graph) [ sentiment == 'no sentiment' ]$color <- "orange"
 #pal <- c("red", "green", "orange", "purple")
 #vertex_colors <- pal[igraph::V(graph)$sentiment]
 
-igraph::V(graph)$membership <- membership(edge.betweenness.community(graph))
 igraph::V(graph)$stress = snafun::v_stress(graph, igraph::V(graph))
 igraph::V(graph)$eccentricity <- igraph::eccentricity(graph, 
                                                       vids = igraph::V(graph),
@@ -29,7 +40,7 @@ plot(
   graph,
   vertex.label = NA,
   edge.arrow.size = .1,
-  vertex.size = igraph::V(graph)$eccentricity/2,
+  vertex.size = 3,
   vertex.color = igraph::V(graph)$color
 )
 
