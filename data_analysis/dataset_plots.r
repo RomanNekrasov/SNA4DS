31345260c6a8818c8b05718d7f22f537283914d0
huub <huubvandevoort@home.nl>
2023-12-08 18:07:36 +0100

    worked on the report Just committed to make sure nothing goes lost :)

--- data_analysis/dataset_plots.r
+++ data_analysis/dataset_plots.r
@@ -1,24 +1,24 @@
 
-fp = file.path("data_preparation/prepared_data/network.graphml")
+fp = file.path("~/Desktop/SNA4DS/6.Project/SNA4DS/data_preparation/prepared_data/network.graphml")
 main_object <- igraph::read_graph(
-  fp,
-#   '/Users/huubvandevoort/Desktop/SNA4DS/6. Project/SNA4DS/data_preparation/prepared_data/network.graphml',
+#  fp,
+   "~/Desktop/SNA4DS/6.Project/SNA4DS/data_preparation/prepared_data/network.graphml",
   format = "graphml"
 )
 
-graph <- main_object
+main_graph <- main_object
 
 # Centralities Plot
 # Save a plot of centrlaity measures (betweenness, closeness, degree, eccentricity)
 png(file="plots/centralities_plot.png", width = 600, height = 1000)
-snafun::plot_centralities(
+(centralities_plot <- snafun::plot_centralities(
   graph,
   measures = c("betweenness", "closeness", "degree", "eccentricity"), 
   directed = TRUE,
   mode = "all",
   k = 3,
   rescaled = FALSE
-)
+))
 dev.off()
 
 
--- data_analysis/plot_graph.R
+++ data_analysis/plot_graph.R
@@ -11,41 +11,27 @@ graph <- main_object
 to_delete <- as.vector(igraph::E(graph)[igraph::E(graph)$edge_weight <= 1])
 filtered_graph <- igraph::delete_edges(graph, to_delete)
 
-
 # only taking biggest component
 # components <- igraph::components(graph)
 # largest_component <- which.max(components$csize)
 # graph <- igraph::induced_subgraph(graph, 
 #                                     vids = which(components$membership == largest_component))
 
-# # Generate colors based on year:
-colrs <- c("#FFE066", "#F25F5C", "#9DBF9E", "#05204A")
 
+igraph::V(graph)$color <- "black"
+igraph::V(graph) [ sentiment == 'negative' ]$color <- "#F25F5C"
+igraph::V(graph) [ sentiment == 'positive' ]$color <- "#9DBF9E"
+igraph::V(graph) [ sentiment == 'neutral' ]$color <- "#FFE066"
 
-igraph::V(graph)$color == "black"
-igraph::V(graph) [ sentiment == 'negative' ]$color <- "#9DBF9E"
-igraph::V(graph) [ sentiment == 'positive' ]$color <- "#FFE066"
-igraph::V(graph) [ sentiment == 'neutral' ]$color <- "#F25F5C"
-igraph::V(graph) [ sentiment == 'no sentiment' ]$color <- "#05204A"
-
-#pal <- c("red", "green", "orange", "purple")
-#vertex_colors <- pal[igraph::V(graph)$sentiment]
-
-igraph::V(graph)$stress = snafun::v_stress(graph, igraph::V(graph))
-igraph::V(graph)$eccentricity <- igraph::eccentricity(graph, 
-                                                      vids = igraph::V(graph),
-                                                      mode = 'all')
-
-
-png(file="plots/network_plot.png", width = 1000, height = 1000)
 plot(
   graph,
   vertex.label = NA,
-  edge.arrow.size = .25,
-  vertex.size = 3,
+  edge.arrow.size = .05,
+  edge.arrow.color = "black",
+  vertex.size = (igraph::V(graph)$total_likes + 100) / 50,
   vertex.color = igraph::V(graph)$color
 )
 
-graphics::legend(x = 0.75, y = -.85, c("Negative","Positive", "Neutral"), pch = 21,
-                 col = "#777777", pt.bg = colrs, pt.cex = 2.5, cex = 1.2, bty = "o", ncol = 1)
-dev.off()
+colrs <- c("#F25F5C","#9DBF9E","#FFE066")
+graphics::legend(x = 0.75, y = -.85, c("Negative","Positive","Neutral"), pch = 21,
+                 col = "#777777", pt.bg = colrs, pt.cex = 1.5, cex = .8, bty = "o", ncol = 1)
