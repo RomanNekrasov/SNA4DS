c5ae0997396cafce305ea11531466c45bf7fa031
oumaimal <oumaimalemhour@gmail.com>
2023-12-08 13:16:53 +0100

    plots code met comments

--- data_analysis/dataset_plots.r
+++ data_analysis/dataset_plots.r
@@ -9,6 +9,7 @@ main_object <- igraph::read_graph(
 graph <- main_object
 
 # Centralities Plot
+# Save a plot of centrlaity measures (betweenness, closeness, degree, eccentricity)
 png(file="plots/centralities_plot.png", width = 600, height = 1000)
 snafun::plot_centralities(
   graph,
@@ -21,7 +22,32 @@ snafun::plot_centralities(
 dev.off()
 
 
-# Degree Plots
+# Degree Distribution Plot
+# Calculate and save a frequency plot of the degree distribution for the entire graph
+degree_distribution <- snafun::g_degree_distribution(graph, 
+    mode = "all", 
+    type = "count")
+
+png(file="plots/degree_distr.png", width = 600, height = 400)
+# Create a frequency plot with lines
+plot(1:length(degree_distribution), degree_distribution, type = "h", lwd = 2, 
+     main = "Degree Distribution", xlab = "Degree", ylab = "Frequency")
+dev.off()
+
+# In Degree Distribution Plot
+# Calculate and save a frequency plot of the in-degree distribution for the graph
+degree_distribution <- snafun::g_degree_distribution(graph, 
+    mode = "in", 
+    type = "count")
+
+png(file="plots/indegree_distr.png", width = 600, height = 400)
+# Create a frequency plot with lines
+plot(1:length(degree_distribution), degree_distribution, type = "h", lwd = 2, 
+     main = "In Degree Distribution", xlab = "inDegree", ylab = "Frequency")
+dev.off()
+
+# Out Degree Distribution Plot
+# Calculate and save a frequency plot of the out-degree distribution for the graph
 degree_distribution <- snafun::g_degree_distribution(graph, 
     mode = "out", 
     type = "count")
@@ -29,6 +55,5 @@ degree_distribution <- snafun::g_degree_distribution(graph,
 png(file="plots/outdegree_distr.png", width = 600, height = 400)
 # Create a frequency plot with lines
 plot(1:length(degree_distribution), degree_distribution, type = "h", lwd = 2, 
-     main = "Degree Distribution", xlab = "outDegree", ylab = "Frequency")
+     main = "Out Degree Distribution", xlab = "outDegree", ylab = "Frequency")
 dev.off()
-
